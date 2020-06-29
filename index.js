02ca3403ebcfb898bec9a731ed9c31c14c227354
Timothy Johnson <timothy.johnson@broadcom.com>
2020-06-29 16:50:15 -0400

    Fix missing async
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-try {
+(async () => {
   await exec.exec("sudo apt-get install -qq -y dbus-x11 gnome-keyring");
   await exec.exec("eval $(dbus-launch --sh-syntax)");
   await exec.exec("echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock");
-  await core.exportVariable("DBUS_SESSION_BUS_ADDRESS", process.env.DBUS_SESSION_BUS_ADDRESS);
-} catch (error) {
+  core.exportVariable("DBUS_SESSION_BUS_ADDRESS", process.env.DBUS_SESSION_BUS_ADDRESS);
+})().catch((error) => {
   core.setFailed(error.message);
-}
+});
