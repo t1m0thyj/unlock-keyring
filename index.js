30cb56ad473feaee924398778bae25da45e06255
Timothy Johnson <timothy.johnson@broadcom.com>
2020-06-29 16:16:50 -0400

    Add commands to unlock keyring
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  await exec.exec("sudo apt-get install -qq -y dbus-x11 gnome-keyring");
+  await exec.exec("eval $(dbus-launch --sh-syntax)");
+  await exec.exec("echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock");
 } catch (error) {
   core.setFailed(error.message);
 }
