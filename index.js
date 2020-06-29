a7c76a621b13061d98c9793497cf83c88d68ec1e
Timothy Johnson <timothy.johnson@broadcom.com>
2020-06-29 16:45:24 -0400

    Export DBUS_SESSION_BUS_ADDRESS
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ try {
   await exec.exec("sudo apt-get install -qq -y dbus-x11 gnome-keyring");
   await exec.exec("eval $(dbus-launch --sh-syntax)");
   await exec.exec("echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock");
+  await core.exportVariable("DBUS_SESSION_BUS_ADDRESS", process.env.DBUS_SESSION_BUS_ADDRESS);
 } catch (error) {
   core.setFailed(error.message);
 }
