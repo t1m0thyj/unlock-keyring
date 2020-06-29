b75f5995c2edafea3d02ef17cdc75dcdbfcd5422
Timothy Johnson <timothy.johnson@broadcom.com>
2020-06-29 17:03:46 -0400

    Log dbus address for testing
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exec = require("@actions/exec");
   await exec.exec("sudo apt-get install -qq -y dbus-x11 gnome-keyring");
   await exec.exec("bash", ["-c", "eval $(dbus-launch --sh-syntax)"]);
   await exec.exec("bash", ["-c", "echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock"]);
+  core.info(process.env.DBUS_SESSION_BUS_ADDRESS);
   core.exportVariable("DBUS_SESSION_BUS_ADDRESS", process.env.DBUS_SESSION_BUS_ADDRESS);
 })().catch((error) => {
   core.setFailed(error.message);
