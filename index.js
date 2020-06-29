461ed721946713057944dc3b0302224e97898373
Timothy Johnson <timothy.johnson@broadcom.com>
2020-06-29 16:54:42 -0400

    Call bash explicitly
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const exec = require("@actions/exec");
 
 (async () => {
   await exec.exec("sudo apt-get install -qq -y dbus-x11 gnome-keyring");
-  await exec.exec("eval $(dbus-launch --sh-syntax)");
-  await exec.exec("echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock");
+  await exec.exec("bash", ["eval $(dbus-launch --sh-syntax)"]);
+  await exec.exec("bash", ["echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock"]);
   core.exportVariable("DBUS_SESSION_BUS_ADDRESS", process.env.DBUS_SESSION_BUS_ADDRESS);
 })().catch((error) => {
   core.setFailed(error.message);
