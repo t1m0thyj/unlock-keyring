edd587b41fbd3be19b2ec7af73377c8e789d49ff
Timothy Johnson <timothy.johnson@broadcom.com>
2020-06-30 07:52:58 -0400

    Extract env vars from command output
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 (async () => {
+  let capturedOutput;
+  const options = {
+    listeners: {
+      stdout: (data) => {
+        capturedOutput += data.toString();
+      }
+    }
+  }
   await exec.exec("sudo apt-get install -qq -y dbus-x11 gnome-keyring");
-  await exec.exec("bash", ["-c", "eval $(dbus-launch --sh-syntax)"]);
-  await exec.exec("bash", ["-c", "echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock"]);
-  core.info(process.env.DBUS_SESSION_BUS_ADDRESS);
-  core.exportVariable("DBUS_SESSION_BUS_ADDRESS", process.env.DBUS_SESSION_BUS_ADDRESS);
+  capturedOutput = "";
+  await exec.exec("bash", ["-c", "dbus-launch --sh-syntax"], options);
+  for (const line of capturedOutput.split("\n")) {
+    //const match = line.match(/(\w+)='?(.+?)'?;/);
+    const match = line.match(/(\w+)=(.+?)/);
+    core.info(`${match[1]}=${match[2]}`);
+    core.exportVariable(match[1], match[2]);
+  }
+  capturedOutput = "";
+  await exec.exec("bash", ["-c", "echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock"], options);
+  for (const line of capturedOutput.split("\n")) {
+    const match = line.match(/(\w+)=(.+?)/);
+    core.info(`${match[1]}=${match[2]}`);
+    core.exportVariable(match[1], match[2]);
+  }
 })().catch((error) => {
   core.setFailed(error.message);
 });
