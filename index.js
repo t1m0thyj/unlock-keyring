e915be8905a26c107e352a3062eeecf8b6682a57
Timothy Johnson <tandrewjohnson@outlook.com>
2021-11-29 12:10:46 -0500

    Update @actions/exec dependency

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 async function execBashCmdAndExportVars(command) {
-  let capturedOutput = "";
-  const options = {
-    listeners: {
-      stdout: (data) => {
-        capturedOutput += data.toString();
-      }
-    }
-  };
-  await exec.exec("bash", ["-c", command], options);
-  for (const line of capturedOutput.split("\n")) {
+  const cmdOutput = await exec.getExecOutput("bash", ["-c", command]);
+  for (const line of cmdOutput.stdout.split("\n")) {
     const match = line.match(/^(\w+)='?(.+?)'?;?$/);
     if (match) core.exportVariable(match[1], match[2]);
   }
@@ -21,6 +13,4 @@ async function execBashCmdAndExportVars(command) {
   await exec.exec("sudo apt-get install -qq dbus-x11 gnome-keyring");
   await execBashCmdAndExportVars("dbus-launch --sh-syntax");
   await execBashCmdAndExportVars("echo 'root' | /usr/bin/gnome-keyring-daemon -r -d --unlock");
-})().catch((error) => {
-  core.setFailed(error.message);
-});
+})().catch(core.setFailed);
